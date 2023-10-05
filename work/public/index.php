3b5e25a3792dedc2edfd41bacb5fac6722834a76
ginkgo <manono2233@gmail.com>
2023-10-05 16:59:10 +0900

    first commit

---
+++ work/public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>My Todos</title>
+</head>
+<body>
+  <h1>Todos</h1>
+
+
+  <a href="./test.html">aaaa</a>
+</body>
+</html>
