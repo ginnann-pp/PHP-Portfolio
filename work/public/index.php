53bc57cf17fd52fa760ea04113b35dc13b431cc4
ginkgo <manono2233@gmail.com>
2023-10-08 16:12:42 +0900

    none

--- work/public/index.php
+++ work/public/index.php
@@ -1,3 +1,47 @@
+<?php
+
+define('DSN', 'mysql:host=db;dbname=php_portfolio;charset=utf8mb4');
+define('DB_USER', 'myappuser');
+define('DB_PASS', 'myapppass');
+// DB設定を定数で指定
+
+try {
+  $pdo = new PDO(DSN, DB_USER, DB_PASS, [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //連想配列
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //例外
+    PDO::ATTR_EMULATE_PREPARES => false, //SQLインジェクション対策
+  ]);
+  echo '接続成功';
+
+} catch (PDOException $e) {
+  echo '接続失敗' . $e->getMessage() . "\n";
+  exit();
+}
+// PDOでphp_portfolio DBに接続
+
+$sql = "SELECT * FROM threads";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// threadsテーブルの中身を抽出
+
+
+
+foreach($data as $val) {
+  echo "<br/>";
+  echo $val ["id"], ":";
+  echo $val ["title"], ":";
+}
+
+
+
+
+
+
+// テーブルの表示
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -15,67 +59,27 @@
 
   <div class="grid">
     <div class="item">
-      <img src="http://placehold.it/240x150" alt="" />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </p>
     </div>
     <div class="item">
-      <img src="http://placehold.it/240x150" alt="" />
       <p>
-        Nulla pharetra porta sagittis. Mauris et suscipit diam. Sed sollicitudin, dui at vulputate varius.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </p>
     </div>
     <div class="item">
-      <img src="http://placehold.it/240x150" alt="" />
       <p>
-        Integer ornare felis eu risus ultrices pharetra eu non velit.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </p>
     </div>
     <div class="item">
-      <img src="http://placehold.it/240x150" alt="" />
       <p>
-        Sed a maximus dui, quis semper felis. Quisque tincidunt dapibus pellentesque.
-      </p>
-    </div>
-    <div class="item">
-      <img src="http://placehold.it/240x150" alt="" />
-      <p>
-        Sed eu sem lacinia, commodo orci non, vulputate neque.
-      </p>
-    </div>
-    <div class="item">
-      <img src="http://placehold.it/240x150" alt="" />
-      <p>
-        Integer ornare felis eu risus ultrices pharetra eu non velit.
-      </p>
-    </div>
-    <div class="item">
-      <img src="http://placehold.it/240x150" alt="" />
-      <p>
-        Integer ornare felis eu risus ultrices pharetra eu non velit.
-      </p>
-    </div>
-    <div class="item">
-      <img src="http://placehold.it/240x150" alt="" />
-      <p>
-        Integer ornare felis eu risus ultrices pharetra eu non velit.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </p>
     </div>
   </div>
 
-  <section class="message-form">
-    <h2>新しいメッセージを投稿</h2>
-    <form action="/post" method="POST">
-      <label for="title">タイトル</label>
-      <input type="text" id="title" name="title" required>
-
-      <label for="content">内容</label>
-      <textarea id="content" name="content" rows="4" required></textarea>
-
-      <button type="submit">投稿</button>
-    </form>
-  </section>
 
 </body>
 
